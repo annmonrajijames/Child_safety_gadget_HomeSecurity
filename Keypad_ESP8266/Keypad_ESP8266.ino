781f0c374913f9f19db047de2b0e04aca3cb7ae3
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-01-14 23:11:35 +0530

    Keypad is actually 4*3, but one row will not be used, due to limited number of I/O pins

--- Keypad_ESP8266/Keypad_ESP8266.ino
+++ Keypad_ESP8266/Keypad_ESP8266.ino
@@ -1,13 +1,15 @@
 #include <Keypad.h>
 
-const byte ROWS = 4; // Four rows
+const byte ROWS = 3; // Three rows
 const byte COLS = 3; // Three columns
 char keys[ROWS][COLS] = {
   {'1','2','3'},
   {'4','5','6'},
-  {'7','8','9'},
+
   {'*','0','#'}
 };
+byte rowPins[ROWS] = {D0, D1, D2}; // Connect keypad ROW0, ROW1, ROW2, and ROW3 to these pins.
+byte colPins[COLS] = {D3, D4, D5}; // Connect keypad COL0, COL1, COL2, and COL3 to these pins.
 
 void setup() {
 Serial.begin(9600); // Initialize serial communication
