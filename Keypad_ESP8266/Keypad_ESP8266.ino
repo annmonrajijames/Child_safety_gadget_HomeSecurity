1676b152472dcf51a719a8a0e53f139fe5161481
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-01-14 22:36:19 +0530

    Keypad library; defined keypad size and layout

--- Keypad_ESP8266/Keypad_ESP8266.ino
+++ Keypad_ESP8266/Keypad_ESP8266.ino
@@ -1,3 +1,14 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; // Four rows
+const byte COLS = 4; // Four columns
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
 void setup() {
 Serial.begin(9600); // Initialize serial communication
 }
