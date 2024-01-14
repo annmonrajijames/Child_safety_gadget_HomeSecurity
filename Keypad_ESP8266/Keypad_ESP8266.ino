078a1a7fe9a4f7a55e342af5c05ac0c209c35d73
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-01-14 22:45:00 +0530

    But Keypad is 3*3

--- Keypad_ESP8266/Keypad_ESP8266.ino
+++ Keypad_ESP8266/Keypad_ESP8266.ino
@@ -1,12 +1,12 @@
 #include <Keypad.h>
 
 const byte ROWS = 4; // Four rows
-const byte COLS = 4; // Four columns
+const byte COLS = 3; // Three columns
 char keys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
 };
 
 void setup() {
