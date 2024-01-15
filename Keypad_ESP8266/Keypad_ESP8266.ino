2f3fc90aeb81448460c1fa70a06f143b7d94f909
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-01-15 12:19:57 +0530

    Buzzer and LED indications based on password entry

--- Keypad_ESP8266/Keypad_ESP8266.ino
+++ Keypad_ESP8266/Keypad_ESP8266.ino
@@ -32,13 +32,41 @@ char key = keypad.getKey(); // Read the key that is pressed
     if (key == '#') {  // Check if '#' is pressed to validate the passcode
       Serial.println("Validate the Password");
       if (v_passcode == "1234#") { // Password is correct
+       Serial.println("Access Granted");
+        digitalWrite(D6, HIGH); // Turn on buzzer
+        digitalWrite(D7, HIGH); // Turn on green LED
+        delay(3000); // Wait for 3 seconds
+        digitalWrite(D6, LOW);  // Turn off buzzer
+        digitalWrite(D7, LOW);  // Turn off green LED
 
       } else if (v_passcode == "5050#") { 
       /* Pseudo-password [From outside of the device it will show correct, 
       but it signals SOS to parents]*/
-
+        digitalWrite(D6, HIGH); // Output from device is same as correct password 
+        digitalWrite(D7, HIGH); 
+        delay(3000); 
+        digitalWrite(D6, LOW);  
+        digitalWrite(D7, LOW);  
       } else { // Password is wrong
-
+        Serial.println("Access Denied");
+        digitalWrite(D6,HIGH);
+        delay(50);
+        digitalWrite(D6,LOW);
+        delay(50);
+        digitalWrite(D6,HIGH);
+        delay(50);
+        digitalWrite(D6,LOW);
+        delay(100);
+        digitalWrite(D6,HIGH);
+        delay(50);
+        digitalWrite(D6,LOW);
+        delay(50);
+        digitalWrite(D6,HIGH);
+        delay(50);
+        digitalWrite(D6,LOW);
+        digitalWrite(D8,HIGH);
+        delay(2000);
+        digitalWrite(D8,LOW);
       }
       Serial.println("The entered password is " + v_passcode);
     }
