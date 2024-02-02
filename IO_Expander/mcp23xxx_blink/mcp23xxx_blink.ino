0aedfa1fb8eb781e1392d8b0ec9a82f5d09b6dde
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-02-02 23:49:41 +0530

    LED blinking for MCP23017

--- IO_Expander/mcp23xxx_blink/mcp23xxx_blink.ino
+++ IO_Expander/mcp23xxx_blink/mcp23xxx_blink.ino
@@ -2,17 +2,17 @@
 
 // ok to include only the one needed
 // both included here to make things simple for example
-#include <Adafruit_MCP23X08.h>
+// #include <Adafruit_MCP23X08.h>
 #include <Adafruit_MCP23X17.h>
 
-#define LED_PIN 0     // MCP23XXX pin LED is attached to
+#define LED_PIN 0     // MCP23XXX pin LED is attached to GPA0; MCP23x17 Pin 21
 
 // only used for SPI
-#define CS_PIN 6
+// #define CS_PIN 6
 
 // uncomment appropriate line
-Adafruit_MCP23X08 mcp;
-//Adafruit_MCP23X17 mcp;
+//Adafruit_MCP23X08 mcp;
+Adafruit_MCP23X17 mcp;
 
 void setup() {
   Serial.begin(9600);
@@ -33,8 +33,8 @@ void setup() {
 }
 
 void loop() {
-  mcp.digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  mcp.digitalWrite(LED_PIN, LOW);
-  delay(500);
+ mcp.digitalWrite(LED_PIN, HIGH);
+ delay(500);
+ mcp.digitalWrite(LED_PIN, LOW);
+ delay(500);
 }
