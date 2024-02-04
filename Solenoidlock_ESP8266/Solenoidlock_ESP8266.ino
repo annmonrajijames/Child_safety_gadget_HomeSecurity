8f5b3fdf5715526e95642d654582ce77ac51da89
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-02-04 15:47:32 +0530

    Left out three PINS for IO_expander but keypad now working properly

--- Solenoidlock_ESP8266/Solenoidlock_ESP8266.ino
+++ Solenoidlock_ESP8266/Solenoidlock_ESP8266.ino
@@ -1,5 +1,7 @@
 #include <Keypad.h>
-
+// #define Buzzer D1
+// #define GreenLed D0
+// #define RedLed D2
 const byte ROWS = 3; // Three rows
 const byte COLS = 3; // Three columns
 char keys[ROWS][COLS] = {
@@ -8,15 +10,15 @@ char keys[ROWS][COLS] = {
 
   {'*','0','#'}
 };
-byte rowPins[ROWS] = {D0, D1, D2}; // Connect keypad ROW0, ROW1 and ROW3 to these pins respectively.
-byte colPins[COLS] = {D3, D4, D5}; // Connect keypad COL0, COL1 and COL2 to these pins respectively.
+byte rowPins[ROWS] = {D3, D4, D5}; // Connect keypad ROW0, ROW1 and ROW3 to these pins respectively.
+byte colPins[COLS] = {D6, D7, D8}; // Connect keypad COL0, COL1 and COL2 to these pins respectively.
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); //Initialize the keypad
 String v_passcode="";
 void setup() {
 Serial.begin(9600); // Start serial communication at 9600 baud rate
-pinMode(D6, OUTPUT); // Buzzer
-pinMode(D7, OUTPUT); // Green LED
-pinMode(D8, OUTPUT); // Red LED
+/*pinMode(Buzzer, OUTPUT); // Buzzer
+pinMode(GreenLed, OUTPUT); // Green LED
+pinMode(RedLed, OUTPUT); // Red LED*/
 }
 
 void loop() {
@@ -33,32 +35,32 @@ char key = keypad.getKey(); // Read the key that is pressed
       Serial.println("Validate the Password");
       if (v_passcode == "1234#") { // Password is correct
        Serial.println("Access Granted");
-        digitalWrite(D6, HIGH); // Turn on buzzer
-        digitalWrite(D7, HIGH); // Turn on green LED
+       // digitalWrite(Buzzer, HIGH); // Turn on buzzer
+       // digitalWrite(GreenLed, HIGH); // Turn on green LED
         delay(3000); // Wait for 3 seconds
-        digitalWrite(D6, LOW);  // Turn off buzzer
-        digitalWrite(D7, LOW);  // Turn off green LED
+       // digitalWrite(Buzzer, LOW);  // Turn off buzzer
+       // digitalWrite(GreenLed, LOW);  // Turn off green LED
 
       } else if (v_passcode == "123#") { 
       /* Pseudo-password [From outside of the device it will show correct, 
       but it signals SOS to parents]*/
-        digitalWrite(D6, HIGH); // Output from device is same as correct password 
-        digitalWrite(D7, HIGH); 
+       // digitalWrite(Buzzer, HIGH); // Output from device is same as correct password 
+      //digitalWrite(GreenLed, HIGH); 
         delay(3000); 
-        digitalWrite(D6, LOW);  
-        digitalWrite(D7, LOW);  
+       // digitalWrite(Buzzer, LOW);  
+       // digitalWrite(GreenLed, LOW);  
       } else { // Password is wrong
         Serial.println("Access Denied");
-         for (int i = 0; i < 3; i++) {
-          digitalWrite(D6, HIGH);
+        /* for (int i = 0; i < 3; i++) {
+          digitalWrite(Buzzer, HIGH);
           delay(50);
-          digitalWrite(D6, LOW);
+          digitalWrite(Buzzer, LOW);
           delay(50);
         }
-          digitalWrite(D8,HIGH);
+          digitalWrite(RedLed,HIGH);
           delay(2000);
-          digitalWrite(D8,LOW);
-      }
+          digitalWrite(RedLed,LOW); */
+      } 
       Serial.println("The entered password is " + v_passcode);
     }
   }
