ef1f2491c0ac2d6cd34521dceee5185b047cd696
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-02-11 20:41:24 +0530

    Added a door lock button in Home screen which will redirect to newly created page called operating page

--- Mobile_app/Child_safety_app/app/src/main/java/com/example/child_safety_app/HomeActivity.java
+++ Mobile_app/Child_safety_app/app/src/main/java/com/example/child_safety_app/HomeActivity.java
@@ -3,6 +3,9 @@ package com.example.child_safety_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Find the button by its ID
+        Button doorLockButton = findViewById(R.id.door_lock_button);
+
+        // Set a click listener on the button
+        doorLockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the OperatingPage activity
+                Intent intent = new Intent(HomeActivity.this, OperatingPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
