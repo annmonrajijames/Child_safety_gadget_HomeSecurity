fd44ad91267c89d429bc9f77a1f64ed86310ff21
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-02-20 17:49:59 +0530

    Named the pins

--- PCF8574/PCF8574.ino
+++ PCF8574/PCF8574.ino
@@ -3,6 +3,10 @@
 #define BLYNK_AUTH_TOKEN "RDs-5TaFIsxVHn6VEC483jSQKw_JjJ0q"
 #define BLYNK_PRINT Serial
 
+#define Buzzer D6
+#define GREEN D7
+#define RED D8
+
 char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "Annmon"; // WiFi network name
 char pass[] = "childsafe"; // WiFi password
@@ -36,9 +40,9 @@ void setup(){
   Blynk.begin(auth, ssid, pass); // Start Blynk using WiFi credentials
   Serial.begin(9600);                     // initialize serial communication
   customKeypad.begin( );                  // initialize matrix keypad
-  pinMode(D6, OUTPUT); // Buzzer
-  pinMode(D7, OUTPUT); // Green LED
-  pinMode(D8, OUTPUT); // Red LED
+  pinMode(Buzzer, OUTPUT); // Buzzer
+  pinMode(GREEN, OUTPUT); // Green LED
+  pinMode(RED, OUTPUT); // Red LED
   timer.setInterval(1L, emailsetup); // Set timer to call emailsetup function every 1 second
 }
   
@@ -57,33 +61,33 @@ void emailsetup() {
       if (v_passcode == "1234#") { // Password is correct
        Blynk.logEvent("password_entry", "Your child reached home"); // correct password entry
        Serial.println("Access Granted");
-        digitalWrite(D6, HIGH); // Turn on buzzer
-        digitalWrite(D7, HIGH); // Turn on green LED
+        digitalWrite(Buzzer, HIGH); // Turn on buzzer
+        digitalWrite(GREEN, HIGH); // Turn on green LED
         delay(3000); // Wait for 3 seconds
-        digitalWrite(D6, LOW);  // Turn off buzzer
-        digitalWrite(D7, LOW);  // Turn off green LED
+        digitalWrite(Buzzer, LOW);  // Turn off buzzer
+        digitalWrite(GREEN, LOW);  // Turn off green LED
 
       } else if (v_passcode == "123#") { 
       /* Pseudo-password [From outside of the device it will show correct, 
       but it signals SOS to parents]*/
         Blynk.logEvent("password_entry", "Your Child is in danger"); // Child entered SOS code
-        digitalWrite(D6, HIGH); // Output from device is same as correct password 
-        digitalWrite(D7, HIGH); 
+        digitalWrite(Buzzer, HIGH); // Output from device is same as correct password 
+        digitalWrite(GREEN, HIGH); 
         delay(3000); 
-        digitalWrite(D6, LOW);  
-        digitalWrite(D7, LOW);  
+        digitalWrite(Buzzer, LOW);  
+        digitalWrite(GREEN, LOW);  
       } else { // Password is wrong
         Blynk.logEvent("password_entry", "Wrong Passcode Entered"); // Wrong password entry
         Serial.println("Access Denied");
          for (int i = 0; i < 3; i++) {
-          digitalWrite(D6, HIGH);
+          digitalWrite(Buzzer, HIGH);
           delay(50);
-          digitalWrite(D6, LOW);
+          digitalWrite(Buzzer, LOW);
           delay(50);
         }
-          digitalWrite(D8,HIGH);
+          digitalWrite(RED,HIGH);
           delay(2000);
-          digitalWrite(D8,LOW);
+          digitalWrite(RED,LOW);
       }
       Serial.println("The entered password is " + v_passcode);
     }
