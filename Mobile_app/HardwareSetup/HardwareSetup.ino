78baaad3804c99554d199619e5b147363534bee3
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-02-26 23:23:15 +0530

    Modified HardwareSetup file accordingly for PCF8574 interface with Firesensor and Keypad

--- Mobile_app/HardwareSetup/HardwareSetup.ino
+++ Mobile_app/HardwareSetup/HardwareSetup.ino
@@ -3,11 +3,11 @@
 #define BLYNK_AUTH_TOKEN "RDs-5TaFIsxVHn6VEC483jSQKw_JjJ0q"
 #define BLYNK_PRINT Serial
 
-#include <ESP8266WiFi.h>
-#include <Keypad.h>
-#include <BlynkSimpleEsp8266.h>
-
-#include <WiFiClient.h>
+#define SolenoidLock D5
+#define Buzzer D6
+#define GREEN D7
+#define RED D8
+#define flameSensor D0
 
 char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "Annmon"; // WiFi network name
@@ -15,26 +15,29 @@ char pass[] = "childsafe"; // WiFi password
 
 #include <String.h>
 
-#define ROW0 D0
-#define ROW1 D1
-#define ROW3 D2
-#define COL0 D3
-#define COL1 D4
-#define COL2 D5
-#define SolenoidLock D6
-#define GREEN D7
-#define RED D8
-const byte ROWS = 3; // Three rows
-const byte COLS = 3; // Three columns
-char keys[ROWS][COLS] = {
-  {'1','2','3'},
-  {'4','5','6'},
+#define I2CADDR         0x20       // matrix keypad address
+#include <ESP8266WiFi.h>
+#include <Keypad.h>               //library for matrix keypad
+#include <Wire.h>                 //library for I2C communication
+#include <Keypad_I2C.h>           //library for matrix keypad I2C communication
+#include <BlynkSimpleEsp8266.h>
+#include <WiFiClient.h>
 
-  {'*','0','#'}
+const byte ROWS = 4;              // 4 rows in matrix keypad
+const byte COLS = 4;              // 4 columns in matrix keypad
+
+char hexaKeys[ROWS][COLS] = { //define a character for every button
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
 };
-byte rowPins[ROWS] = {ROW0, ROW1, ROW3}; // Connect keypad ROW0, ROW1 and ROW3 to these pins respectively.
-byte colPins[COLS] = {COL0, COL1, COL2}; // Connect keypad COL0, COL1 and COL2 to these pins respectively.
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); //Initialize the keypad
+
+byte rowPins[ROWS] = {0, 1, 2, 3}; // row pins connected to I2C module 
+byte colPins[COLS] = {4, 5, 6, 7}; // column pins connected to I2C module 
+
+// object for matrix keypad
+Keypad_I2C customKeypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS, I2CADDR); 
 String v_passcode="";
 BlynkTimer timer; // timer object created, BlynkTimer is a class.
 
@@ -42,57 +45,60 @@ const char* ap_ssid = "Annmon_ESP_Hotspot";
 const char* ap_password = "childsafe";
 WiFiServer server(80);
 
-void setup() {
-Blynk.begin(auth, ssid, pass); // Start Blynk using WiFi credentials
-Serial.begin(9600);
-pinMode(SolenoidLock, OUTPUT); // Pin connected to the relay controlling the solenoid lock
-digitalWrite(SolenoidLock, LOW); // Start with the solenoid lock in locked state
-pinMode(GREEN, OUTPUT); // Green LED
-pinMode(RED, OUTPUT); // Red LED
-timer.setInterval(1L, emailsetup); // Set timer to call emailsetup function every 1 second
-
-
+void setup(){
+  Blynk.begin(auth, ssid, pass); // Start Blynk using WiFi credentials
+  Serial.begin(9600);                     // initialize serial communication
+  customKeypad.begin( );                  // initialize matrix keypad
+  pinMode(SolenoidLock, OUTPUT); // Pin connected to the relay controlling the solenoid lock
+  digitalWrite(SolenoidLock, LOW); // Start with the solenoid lock in locked state
+  pinMode(Buzzer, OUTPUT); // Buzzer
+  pinMode(GREEN, OUTPUT); // Green LED
+  pinMode(RED, OUTPUT); // Red LED
+  timer.setInterval(1L, emailsetup); // Set timer to call emailsetup function every 1 second
   WiFi.softAP(ap_ssid, ap_password); 
   server.begin();
   Serial.println("Access Point started");
   Serial.print("IP address: ");
-  Serial.println(WiFi.softAPIP());  //  
-
-  pinMode(SolenoidLock, OUTPUT);
-
-  digitalWrite(SolenoidLock, LOW);
+  Serial.println(WiFi.softAPIP());
+  pinMode(flameSensor, INPUT); // Set the flame sensor pin as an input
 }
-// emailsetup being scheduled to be called repeatedly by the BlynkTimer object.
-// emailsetup() is scheduled to run every 1 millisecond, almost every time loop() runs, timer.run() will execute emailsetup().
+bool isFlameDetected() {
+  int flameDetected = digitalRead(flameSensor); // Read the flame sensor value
+  return flameDetected == LOW; // Return true if flame is detected
+}  
 void emailsetup() {
-char key = keypad.getKey(); // Read the key that is pressed
-    if (key != NO_KEY) { // If a key is pressed, add it to the passcode string, NO_KEY is predefined in they keypad library
+  char key = customKeypad.getKey();  // save character pressed 
+  if (key != NO_KEY) { // If a key is pressed, add it to the passcode string, NO_KEY is predefined in they keypad library
     v_passcode = v_passcode + key; // Append the pressed key to the passcode
 
-    if (key == '*') { // Check if '*' is pressed to reset passcode entry
+    if (key == 'A') { // Check if 'A' is pressed to reset passcode entry
       Serial.println("Enter Password");
       v_passcode = ""; // Reset the passcode
     }
 
-    if (key == '#') {  // Check if '#' is pressed to validate the passcode
+    if (key == 'D') {  // Check if 'D' is pressed to validate the passcode
       Serial.println("Validate the Password");
-      if (v_passcode == "1234#") { // Password is correct
+      if (v_passcode == "1234D") { // Password is correct
        Blynk.logEvent("password_entry", "Your child reached home"); // correct password entry
        Serial.println("Access Granted");
         digitalWrite(SolenoidLock, HIGH); // Unlock the solenoid lock
+        digitalWrite(Buzzer, HIGH); // Turn on buzzer
         digitalWrite(GREEN, HIGH); // Turn on green LED
         delay(3000); // Wait for 3 seconds
+        digitalWrite(Buzzer, LOW);  // Turn off buzzer
         digitalWrite(GREEN, LOW);  // Turn off green LED
 
-      } else if (v_passcode == "123#") { 
+      } else if (v_passcode == "123D") { 
       /* Pseudo-password [From outside of the device it will show correct, 
       but it signals SOS to parents]*/
-       Blynk.logEvent("password_entry", "Your Child is in danger"); // Child entered SOS code
+        Blynk.logEvent("password_entry", "Your Child is in danger"); // Child entered SOS code
         digitalWrite(SolenoidLock, HIGH); // Unlock the solenoid lock
-        digitalWrite(GREEN, HIGH); // Turn on green LED
-        delay(3000);  
-        digitalWrite(GREEN, LOW);  // Turn on green LED
-      } else if (v_passcode == "4321#") { // To Unlock the door
+        digitalWrite(Buzzer, HIGH); // Output from device is same as correct password 
+        digitalWrite(GREEN, HIGH); 
+        delay(3000); 
+        digitalWrite(Buzzer, LOW);  
+        digitalWrite(GREEN, LOW);  
+      } else if (v_passcode == "4321D") { // To Unlock the door
         Blynk.logEvent("password_entry", "Door is Locked now");
         Serial.println("\nDoor is Locked");
         digitalWrite(SolenoidLock, LOW); // Lock the solenoid lock
@@ -104,22 +110,31 @@ char key = keypad.getKey(); // Read the key that is pressed
         delay(100); // Wait for 1/10 second
         digitalWrite(RED, LOW); // Red LED Off 
       } else { // Password is wrong
-      Blynk.logEvent("password_entry", "Wrong Passcode Entered"); // Wrong password entry
+        Blynk.logEvent("password_entry", "Wrong Passcode Entered"); // Wrong password entry
         Serial.println("Access Denied");
-          digitalWrite(RED,HIGH); // RED led
+         for (int i = 0; i < 3; i++) {
+          digitalWrite(Buzzer, HIGH);
+          delay(50);
+          digitalWrite(Buzzer, LOW);
+          delay(50);
+        }
+          digitalWrite(RED,HIGH);
           delay(2000);
-          digitalWrite(RED,LOW); // RED led
+          digitalWrite(RED,LOW);
       }
       Serial.println("The entered password is " + v_passcode);
-      v_passcode = ""; // Reset the passcode string
     }
   }
-  
+  if (isFlameDetected()) {
+    Blynk.logEvent("password_entry", "Door is on Fire");
+    Serial.println("Flame detected!");    // Flame is detected
+  }
 }
+
 void loop() {
   Blynk.run(); // Run Blynk process
   timer.run(); // checks if it's time to call any function scheduled by the timer.
-  String all_command = "";
+   String all_command = "";
 
   WiFiClient client = server.available();
 
@@ -182,5 +197,5 @@ void loop() {
         }
       }
     }
-  }
+  } 
 }
