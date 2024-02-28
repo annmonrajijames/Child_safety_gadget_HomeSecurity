52352bff7862844f231ef8179caa601967dbcd50
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-02-28 20:09:41 +0530

    Basic code to test whether solenoid lock is working

--- InsideLock/InsideLock.ino
+++ InsideLock/InsideLock.ino
@@ -1,9 +1,12 @@
 void setup() {
-  // put your setup code here, to run once:
-
+pinMode(D6, OUTPUT); // Pin connected to the relay controlling the solenoid lock
+digitalWrite(D6, LOW); // Start with the solenoid lock in locked state
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+digitalWrite(D6, HIGH);
+delay(2000);
+digitalWrite(D6, LOW);
+delay(2000);
 }
