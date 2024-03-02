9baa1355709a8f4d279ba3ed51cbe7d569f5f2f4
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-03-02 22:16:01 +0530

    No need to press # button as OK button after entering the passcode, it will automatically submit if the password is correct

--- ForgotPassword/ForgotPassword.ino
+++ ForgotPassword/ForgotPassword.ino
@@ -1,48 +1,48 @@
-#include <ESP8266WiFi.h>
 #include <Keypad.h>
 
-const int numRows = 4; // Number of rows on the keypad
-const int numCols = 3; // Number of columns on the keypad
+const byte ROWS = 4; // Four rows
+const byte COLS = 3; // Three columns
 
-char keys[numRows][numCols] = {
+// Define the Keymap
+char keys[ROWS][COLS] = {
   {'1','2','3'},
   {'4','5','6'},
   {'7','8','9'},
   {'*','0','#'}
 };
 
-byte rowPins[numRows] = {D0, D1, D2, D3}; // Connect to the row pinouts of the keypad
-byte colPins[numCols] = {D4, D5, D6};      // Connect to the column pinouts of the keypad
+// Connect to the row pinouts of the keypad
+byte rowPins[ROWS] = {D0, D1, D2, D3};
+// Connect to the column pinouts of the keypad
+byte colPins[COLS] = {D4, D5, D6};
 
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, numRows, numCols );
+// Initialize the keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-String password = "1234"; // Set your password here
-String enteredPassword = "";
+String password = "1234"; // Example password
+String enteredPassword;
 
 void setup(){
   Serial.begin(9600);
-  Serial.println("Enter Password:");
 }
 
 void loop(){
   char key = keypad.getKey();
-  
-  if(key){
-    if(key == '#'){
-      if(enteredPassword == password){
-        Serial.println("Access granted!");
-        Serial.println("Enter Password:");
-        enteredPassword = "";
+
+  // If a key is pressed, append it to the enteredPassword
+  if (key != NO_KEY){
+    Serial.print(key);
+    enteredPassword += key;
+
+    // Check if the entered password is correct after reaching the password length
+    if (enteredPassword.length() == password.length()) {
+      if (enteredPassword == password) {
+        Serial.println("\nPassword Correct");
+        // Add actions here (unlock door, turn on light, etc.)
+      } else {
+        Serial.println("\nPassword Incorrect");
       }
-      else{
-        Serial.println("Access denied!");
-        Serial.println("Enter Password:");
-        enteredPassword = "";
-      }
-    }
-    else{
-      enteredPassword += key;
-      Serial.print(key);
+      enteredPassword = ""; // Reset entered password after each attempt
     }
   }
 }
