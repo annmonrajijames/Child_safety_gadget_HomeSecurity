c9d4f6eba1c0dfd0798614b0746a59f6331e7db7
Annmon James <annmonjameschiramel@gmail.com>
2024-03-03 17:51:45 +0530

    Revert "Forgot password"

---
+++ Mobile_app/SoftwareSetup/app/src/main/java/com/example/you_tube_wifi_app/OperatingPage.java
@@ -0,0 +1,116 @@
+package com.example.you_tube_wifi_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class OperatingPage extends AppCompatActivity {
+
+    Button btnGREEN, btnBLUE, btnRED;
+    TextView txtRES;
+
+    private OkHttpClient client = new OkHttpClient();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.operating_page);
+
+
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkRequest.Builder builder = new NetworkRequest.Builder();
+        builder.addTransportType(NetworkCapabilities.TRANSPORT_WIFI);
+        builder.addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
+        NetworkRequest request = builder.build();
+        connManager.requestNetwork(request, new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(Network network) {
+                connManager.bindProcessToNetwork(network);
+            }
+        });
+
+
+        btnBLUE = findViewById(R.id.btnBLUE);
+        btnGREEN = findViewById(R.id.btnGREEN);
+        btnRED = findViewById(R.id.btnRED);
+        txtRES = findViewById(R.id.txtRES);
+
+
+        btnRED.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendCommand("red");
+            }
+        });
+
+
+        btnGREEN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendCommand("green");
+
+            }
+        });
+
+
+        btnBLUE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendCommand("blue");
+
+            }
+        });
+
+
+    }
+
+    public void sendCommand(String cmd) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String command = "http://192.168.66.198/" + cmd;
+                Log.d("Command------------------------------------------", command);
+                Request request = new Request.Builder().url(command).build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    String myResponse = response.body().string();
+                    final String cleanResponse = myResponse.replaceAll("\\<.*?\\>", ""); // remove HTML tags
+                    cleanResponse.replace("\n", ""); // remove all new line characters
+                    cleanResponse.replace("\r", ""); // remove all carriage characters
+                    cleanResponse.replace(" ", ""); // removes all space characters
+                    cleanResponse.replace("\t", ""); // removes all tab characters
+                    cleanResponse.trim();
+                    Log.d("Response  = ", cleanResponse);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            txtRES.setText(cleanResponse);
+                        }
+                    });
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+
+}
